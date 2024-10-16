cbdf1ee7d4e3271255c92a488e727b7542bec138
Thassiana Camilia Amorim Muller <thassi.ana.amorim@gmail.com>
2024-10-16 20:11:40 -0300

    fix css

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,7 +31,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: "Todo was successfully created." }
+        format.html { redirect_to @todo}
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -57,7 +57,7 @@ class TodosController < ApplicationController
     @todo.destroy
 
     respond_to do |format|
-      format.html { redirect_to todos_path, status: :see_other, notice: "Todo was successfully destroyed." }
+      format.html { redirect_to todos_path, status: :see_other}
       format.json { head :no_content }
     end
   end
